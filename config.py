98245707d3150ef97d7ca7b4cd54828c8a7d9a1b
g94213974-byte <g94213974@gmail.com>
2026-06-20 12:18:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))
